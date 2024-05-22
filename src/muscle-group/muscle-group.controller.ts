07fde345be8e59c53ebfbb4fcb67108f26ebe0d0
David Lima <antdavidlima@gmail.com>
2024-05-22 02:44:15 -0300

    feat(muscle-group): create endpoint to delete and update muscle groups

--- src/muscle-group/muscle-group.controller.ts
+++ src/muscle-group/muscle-group.controller.ts
@@ -2,9 +2,14 @@ import {
 	BadRequestException,
 	Body,
 	Controller,
+	Delete,
 	ForbiddenException,
 	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
 	Post,
+	Put,
 	UseGuards,
 } from '@nestjs/common';
 import { AuthenticationGuard } from 'src/authentication/authentication.guard';
@@ -19,6 +24,32 @@ const createMuscleGroupBodySchema = z.object({
 });
 
 type CreateMuscleGroupBodySchema = z.infer<typeof createMuscleGroupBodySchema>;
+
+const deleteMuscleGroupParamsSchema = z.object({
+	id: z.coerce.number({
+		required_error: 'Não foi possível identificar o grupo muscular',
+	}),
+});
+
+type DeleteMuscleGroupParamsSchema = z.infer<
+	typeof deleteMuscleGroupParamsSchema
+>;
+
+const updateMuscleGroupParamsSchema = z.object({
+	id: z.coerce.number({
+		required_error: 'Não foi possível identificar o grupo muscular',
+	}),
+});
+
+type UpdateMuscleGroupParamsSchema = z.infer<
+	typeof updateMuscleGroupParamsSchema
+>;
+
+const updateMuscleGroupBodySchema = z.object({
+	name: z.string({ required_error: "O campo 'Nome' é obrigatório" }),
+});
+
+type UpdateMuscleGroupBodySchema = z.infer<typeof updateMuscleGroupBodySchema>;
 
 @Controller('muscle-group')
 @UseGuards(AuthenticationGuard)
@@ -96,5 +127,103 @@ export class MuscleGroupController {
 				name: true,
 			},
 		});
+	}
+
+	@Delete(':id')
+	@HttpCode(HttpStatus.NO_CONTENT)
+	async destroy(
+		@Param(new ZodValidationPipe(deleteMuscleGroupParamsSchema))
+		{ id }: DeleteMuscleGroupParamsSchema,
+		@AuthenticationTokenPayload()
+		authenticationTokenPayload: AuthenticationTokenPayloadSchema,
+	) {
+		const currentUser = await this.prismaService.user.findUnique({
+			where: {
+				id: authenticationTokenPayload.sub,
+			},
+			select: {
+				isProfessor: true,
+			},
+		});
+
+		if (!currentUser) {
+			throw new BadRequestException(
+				'Não foi possível identificar o usuário logado',
+			);
+		}
+
+		if (!currentUser.isProfessor) {
+			throw new ForbiddenException(
+				'Apenas professores podem deletar grupos musculares',
+			);
+		}
+
+		const muscleGroup = await this.prismaService.muscleGroup.findUnique({
+			where: {
+				id,
+			},
+		});
+
+		if (!muscleGroup) {
+			throw new BadRequestException('Grupo muscular não encontrado');
+		}
+
+		await this.prismaService.muscleGroup.delete({
+			where: {
+				id,
+			},
+		});
+	}
+
+	@Put(':id')
+	async update(
+		@Param(new ZodValidationPipe(updateMuscleGroupParamsSchema))
+		{ id }: UpdateMuscleGroupParamsSchema,
+		@Body(new ZodValidationPipe(updateMuscleGroupBodySchema))
+		{ name }: UpdateMuscleGroupBodySchema,
+		@AuthenticationTokenPayload()
+		authenticationTokenPayload: AuthenticationTokenPayloadSchema,
+	) {
+		const currentUser = await this.prismaService.user.findUnique({
+			where: {
+				id: authenticationTokenPayload.sub,
+			},
+			select: {
+				isProfessor: true,
+			},
+		});
+
+		if (!currentUser) {
+			throw new BadRequestException(
+				'Não foi possível identificar o usuário logado',
+			);
+		}
+
+		if (!currentUser.isProfessor) {
+			throw new ForbiddenException(
+				'Apenas professores podem alterar grupos musculares',
+			);
+		}
+
+		const muscleGroup = await this.prismaService.muscleGroup.findUnique({
+			where: {
+				id,
+			},
+		});
+
+		if (!muscleGroup) {
+			throw new BadRequestException('Grupo muscular não encontrado');
+		}
+
+		const updatedMuscleGroup = await this.prismaService.muscleGroup.update({
+			where: {
+				id,
+			},
+			data: {
+				name,
+			},
+		});
+
+		return updatedMuscleGroup;
 	}
 }
