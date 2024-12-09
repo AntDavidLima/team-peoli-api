dd97433b646b61d4e0e78d43ee155504a50f54a6
David Lima <antdavidlima@gmail.com>
2024-12-09 07:20:47 -0300

    feat(exercise): add query parameters for filtering exercises by date range

--- src/user/exercise/exercise.controller.ts
+++ src/user/exercise/exercise.controller.ts
@@ -1,10 +1,11 @@
 import {
-	BadRequestException,
-	Controller,
-	ForbiddenException,
-	Get,
-	Param,
-	UseGuards,
+  BadRequestException,
+  Controller,
+  ForbiddenException,
+  Get,
+  Param,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthenticationGuard } from 'src/authentication/authentication.guard';
 import { AuthenticationTokenPayloadSchema } from 'src/authentication/authentication.strategy';
@@ -14,110 +15,129 @@ import { ZodValidationPipe } from 'src/zod-validation/zod-validation.pipe';
 import { z } from 'zod';
 
 const listExercisesParamsSchema = z.object({
-	id: z.coerce.number(),
+  id: z.coerce.number(),
 });
 
 type ListExercisesParamsSchema = z.infer<typeof listExercisesParamsSchema>;
 
+const listExercisesQueryParamsSchema = z.object({
+  startDate: z.coerce.date().optional(),
+  endDate: z.coerce.date().optional(),
+});
+
+type ListExercisesQueryParamsSchema = z.infer<
+  typeof listExercisesQueryParamsSchema
+>;
+
 @Controller('user/:id/exercise')
 @UseGuards(AuthenticationGuard)
 export class ExerciseController {
-	constructor(private prismaService: PrismaService) { }
+  constructor(private prismaService: PrismaService) {}
 
-	@Get()
-	async index(
-		@AuthenticationTokenPayload()
-		authenticationTokenPayload: AuthenticationTokenPayloadSchema,
-		@Param(new ZodValidationPipe(listExercisesParamsSchema))
-		{ id }: ListExercisesParamsSchema,
-	) {
-		const currentUser = await this.prismaService.user.findUnique({
-			where: {
-				id: authenticationTokenPayload.sub,
-			},
-			select: {
-				isProfessor: true,
-				id: true,
-			},
-		});
+  @Get()
+  async index(
+    @AuthenticationTokenPayload()
+    authenticationTokenPayload: AuthenticationTokenPayloadSchema,
+    @Param(new ZodValidationPipe(listExercisesParamsSchema))
+    { id }: ListExercisesParamsSchema,
+    @Query(new ZodValidationPipe(listExercisesQueryParamsSchema))
+    { startDate, endDate }: ListExercisesQueryParamsSchema,
+  ) {
+    const currentUser = await this.prismaService.user.findUnique({
+      where: {
+        id: authenticationTokenPayload.sub,
+      },
+      select: {
+        isProfessor: true,
+        id: true,
+      },
+    });
 
-		if (!currentUser) {
-			throw new BadRequestException(
-				'Não foi possível identificar o usuário logado',
-			);
-		}
+    if (!currentUser) {
+      throw new BadRequestException(
+        'Não foi possível identificar o usuário logado',
+      );
+    }
 
-		if (currentUser.id !== id && !currentUser.isProfessor) {
-			throw new ForbiddenException(
-				'Somente professores podem visualizar os exercícios de outros usuários',
-			);
-		}
+    if (currentUser.id !== id && !currentUser.isProfessor) {
+      throw new ForbiddenException(
+        'Somente professores podem visualizar os exercícios de outros usuários',
+      );
+    }
 
-		const user = await this.prismaService.user.findUnique({
-			where: {
-				id,
-			},
-		});
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    });
 
-		if (!user) {
-			throw new BadRequestException('Usuário não encontrado');
-		}
+    if (!user) {
+      throw new BadRequestException('Usuário não encontrado');
+    }
 
-		return this.prismaService.exercise.findMany({
-			where: {
-				trainings: {
-					every: {
-						training: {
-							routines: {
-								every: {
-									userId: id,
-								},
-							},
-						},
-					},
-				},
-				workouts: {
-					some: {
-						WorkoutExerciseSets: {
-							some: {
-								AND: {
-									reps: {
-										not: undefined,
-									},
-									load: {
-										not: undefined,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-			select: {
-				id: true,
-				name: true,
-				workouts: {
-					select: {
-						WorkoutExerciseSets: {
-							select: {
-								id: true,
-								load: true,
-								reps: true,
-							},
-						},
-						workout: {
-							select: {
-								startTime: true,
-							},
-						},
-					},
-					orderBy: {
-						workout: {
-							startTime: 'asc',
-						},
-					},
-				},
-			},
-		});
-	}
+    return this.prismaService.exercise.findMany({
+      where: {
+        trainings: {
+          every: {
+            training: {
+              routines: {
+                every: {
+                  userId: id,
+                },
+              },
+            },
+          },
+        },
+        workouts: {
+          some: {
+            WorkoutExerciseSets: {
+              some: {
+                AND: {
+                  reps: {
+                    not: undefined,
+                  },
+                  load: {
+                    not: undefined,
+                  },
+                },
+              },
+            },
+          },
+          every: {
+            workout: {
+              startTime: {
+                gte: startDate,
+                lte: endDate,
+              },
+            },
+          },
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        workouts: {
+          select: {
+            WorkoutExerciseSets: {
+              select: {
+                id: true,
+                load: true,
+                reps: true,
+              },
+            },
+            workout: {
+              select: {
+                startTime: true,
+              },
+            },
+          },
+          orderBy: {
+            workout: {
+              startTime: 'asc',
+            },
+          },
+        },
+      },
+    });
+  }
 }
