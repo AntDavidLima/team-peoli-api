9582469cdcb2c83f4202d5020d8959f993f2aec3
David Lima <antdavidlima@gmail.com>
2025-05-06 10:03:52 -0300

    fix(cors): allow request from all address ending with teampeoli.com
    Signed-off-by: David Lima <antdavidlima@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,35 +2,35 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import {
-	FastifyAdapter,
-	NestFastifyApplication,
+  FastifyAdapter,
+  NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
 import { Env } from './env';
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestFastifyApplication>(
-		AppModule,
-		new FastifyAdapter(),
-	);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
-	const config = new DocumentBuilder()
-		.setTitle('Team Peoli')
-		.setDescription('The Team Peoli API definition')
-		.setVersion('1.0')
-		.addTag('enpoints')
-		.build();
+  const config = new DocumentBuilder()
+    .setTitle('Team Peoli')
+    .setDescription('The Team Peoli API definition')
+    .setVersion('1.0')
+    .addTag('enpoints')
+    .build();
 
-	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-	const configService = app.get<ConfigService<Env, true>>(ConfigService);
+  const configService = app.get<ConfigService<Env, true>>(ConfigService);
 
-	app.enableCors({
-		origin: configService.get('CLIENT_URL'),
-	});
+  app.enableCors({
+    origin: new RegExp(configService.get('CLIENT_URL')),
+  });
 
-	await app.listen(configService.get('APPLICATION_PORT'), '0.0.0.0');
+  await app.listen(configService.get('APPLICATION_PORT'), '0.0.0.0');
 }
 
 bootstrap();
