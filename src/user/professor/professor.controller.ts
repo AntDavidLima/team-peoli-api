97cf9e46c02ca2f55a3cf5d2a99e02462f743bd4
BergSouza <bergson2000@live.com>
2025-06-14 16:28:46 -0300

    refactor: add id param in professor controller

--- src/user/professor/professor.controller.ts
+++ src/user/professor/professor.controller.ts
@@ -3,19 +3,20 @@ import {
 	Controller,
 	Get,
 	UseGuards,
+	Param
 } from '@nestjs/common';
 import { AuthenticationGuard } from 'src/authentication/authentication.guard';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-@Controller('/user/professor')
+@Controller('/user/:id/professor')
 @UseGuards(AuthenticationGuard)
 export class ProfessorController {
 	constructor(
 		private prismaService: PrismaService,
 	) { }
 
-	@Get('')
-	async getProf() {
+	@Get()
+	async index(@Param('id') id: number) {
 		const prof = await this.prismaService.user.findFirst({
 			where: {
 				isProfessor: true,
