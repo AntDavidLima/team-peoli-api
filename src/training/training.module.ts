d269cd52f6ae90342f912a82d95450721baa8e9f
BergSouza <bergson2000@live.com>
2025-07-20 15:28:01 -0300

    refactor: replace usernote controller for exercisecontroller in training.modules

--- src/training/training.module.ts
+++ src/training/training.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TrainingController } from './training.controller';
-import { UserNoteController } from './exercise/exercise-user-note.controller';
+import { UserNoteController } from './exercise/exercise.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
