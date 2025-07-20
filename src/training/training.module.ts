264c0a08a704011f83b9d5c93a2b0ecf27e4cdbc
BergSouza <bergson2000@live.com>
2025-07-20 15:18:59 -0300

    feat: add training.module.ts

---
+++ src/training/training.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TrainingController } from './training.controller';
+import { UserNoteController } from './exercise/exercise-user-note.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+	controllers: [TrainingController, UserNoteController ],
+	imports: [PrismaModule],
+})
+export class TrainingModule { }
